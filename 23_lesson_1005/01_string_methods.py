aec5fe16f0d87ca4e69e3d1441d2ebf054dcdada
demidraksin <greatraksin@icloud.com>
2022-05-10 11:42:48 +0300

    Adding strings theory

---
+++ 23_lesson_1005/01_string_methods.py
@@ -0,0 +1,8 @@
+string = 'Hello! This is my first string'  # строчка
+
+# разрезание строк (указывается начальный и конечный индекс разрезания)
+print(string[0:6])  # Hello!
+print(string[7:14])  # This is
+print(string[:6])  # Hello!
+print(string[24:])  # string
+
